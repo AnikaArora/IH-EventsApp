e9896b60929a2f592dadc2c7675851d868066bf1
Anika Arora <anika_arora@ucsb.edu>
2021-07-29 10:08:20 -0700

    fixed map markers

--- app/src/main/java/com/example/eventful/MainActivity.kt
+++ app/src/main/java/com/example/eventful/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.eventful
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -21,9 +22,18 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.coroutines.processNextEventInCurrentThread
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback {
-    private lateinit var mMap: GoogleMap
+class EventDetails {
+    lateinit var title: String
+    lateinit var address: String
+    lateinit var longitude: String
+    lateinit var latitude: String
+}
+
+var listOfEvents = mutableListOf<EventDetails>()
+
+class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,27 +53,14 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         navView.setupWithNavController(navController)
 
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
-
-
 
         val textView = findViewById<TextView>(R.id.text)
         // ...
 
         // Instantiate the RequestQueue.
         val queue = Volley.newRequestQueue(this)
-        val city = "Sacramento"
-        val url = "https://app.ticketmaster.com/discovery/v2/events.json?city=$city&apikey={apikey}"
-
-        class EventDetails {
-            lateinit var title: String
-            lateinit var address: String
-        }
-
-        var listOfEvents = mutableListOf<EventDetails>()
+        val city = "Denver"
+        val url = "https://app.ticketmaster.com/discovery/v2/events.json?city=$city&apikey=t3Pw9IXSSkl3TrReE9v2nrm238YfhoXb"
 
 
         // Request a string response from the provided URL.
@@ -73,45 +70,28 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                 // Display the first 500 characters of the response string.
                 val jsonObject = response.getJSONObject("_embedded")
                 val eventsArr = jsonObject.getJSONArray("events")
+
                 for(i in 0 until eventsArr.length()) {
                     val event = eventsArr.getJSONObject(i)
                     val eventInstance = EventDetails()
-                    val addressObject = event.getJSONObject("_embedded").getJSONObject("venues")
+                    val addressObject = event.getJSONObject("_embedded").getJSONArray("venues").getJSONObject(0)
+
 
                     eventInstance.title = event.getString("name")
-                    eventInstance.address = addressObject.getJSONObject("address").getString("line1")
+                    eventInstance.address = addressObject.getJSONObject("address").optString("line1")
+                    val longitude = addressObject.getJSONObject("location").optString("longitude")
+                    val latitude = addressObject.getJSONObject("location").optString("latitude")
+                    eventInstance.longitude = longitude
+                    eventInstance.latitude = latitude
 
                     listOfEvents.add(eventInstance)
+
                 }
 
             },
-            {  })
+            { error -> Log.d("error", error.toString()) })
+            queue.add(jsonRequest)
 
-        // Add the request to the RequestQueue.
-//        queue.add(JsonObjectRequest)
     }
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-    }
-
-//    val rect1=findViewById<Button>(R.id.rect1);
-//    val rect2=findViewById<Button>(R.id.rect2);
-//    val rect3=findViewById<Button>(R.id.rect3);
 
 }
--- app/src/main/java/com/example/eventful/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/eventful/ui/home/HomeFragment.kt
@@ -8,14 +8,22 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.example.eventful.EventDetails
 import com.example.eventful.R
 import com.example.eventful.databinding.FragmentHomeBinding
+import com.example.eventful.listOfEvents
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var homeViewModel: HomeViewModel
     private var _binding: FragmentHomeBinding? = null
-
+    private lateinit var mMap: GoogleMap
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
@@ -25,21 +33,41 @@ class HomeFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
+
+
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(this)
+
         homeViewModel =
                 ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+
+        for(i in listOfEvents.indices) {
+            val longitude = listOfEvents[i].longitude
+            val latitude = listOfEvents[i].latitude
+            if (!longitude.isNullOrEmpty() && !latitude.isNullOrEmpty()) {
+                val location = LatLng(latitude.toDouble(), longitude.toDouble())
+                mMap.addMarker(MarkerOptions().position(location).title("Marker in location"))
+            }
+        }
+
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val losangeles = LatLng(34.0522, -118.2437)
+        mMap.addMarker(MarkerOptions().position(losangeles).title("Marker in LA"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(losangeles))
     }
 }
