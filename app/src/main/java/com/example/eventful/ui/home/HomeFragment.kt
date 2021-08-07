4eb69009797b16773a5b1e20f5523b14baee5f96
danie <m8w2b@ugrad.cs.ubc.ca>
2021-08-07 16:14:30 -0700

    removed map fragment

--- app/src/main/java/com/example/eventful/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/eventful/ui/home/HomeFragment.kt
@@ -29,13 +29,12 @@ import com.squareup.picasso.Picasso
 import org.json.JSONObject
 
 
-class HomeFragment : Fragment(), OnMapReadyCallback {
+class HomeFragment : Fragment() {
 
     private var API: String = "991c4746ce85b4c70a462313962cd22a"
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var homeViewModel: HomeViewModel
     private var _binding: FragmentHomeBinding? = null
-    private lateinit var mMap: GoogleMap
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
@@ -46,14 +45,7 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
             savedInstanceState: Bundle?
     ): View? {
 
-        var mapFragment = activity?.supportFragmentManager?.findFragmentById(R.id.map) as SupportMapFragment?
-        Log.d("mapFragment", "mapFragment = $mapFragment")
-        mapFragment?.getMapAsync(this)
-        if (mapFragment == null) {
-            mapFragment = SupportMapFragment.newInstance()
-            childFragmentManager.beginTransaction().replace(R.id.map, mapFragment).commit()
-        }
-
+        Log.d("CreateView", "New view created")
         homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
@@ -62,7 +54,9 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
 
         val v : View = root.findViewById(R.id.fragment_home_container)!!
 
-        init(this.requireContext(), v)
+        if (savedInstanceState == null) {
+            init(this.requireContext(), v)
+        }
 
         return root
     }
@@ -72,31 +66,13 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
         _binding = null
     }
 
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-
-        // Add a marker in Sydney and move the camera
-        val losangeles = LatLng(34.0522, -118.2437)
-        mMap.addMarker(MarkerOptions().position(losangeles).title("Marker in LA"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(losangeles))
-
-        for(i in listOfEvents.indices) {
-            val longitude = listOfEvents[i].longitude
-            val latitude = listOfEvents[i].latitude
-            if (!longitude.isNullOrEmpty() && !latitude.isNullOrEmpty()) {
-                val location = LatLng(latitude.toDouble(), longitude.toDouble())
-                mMap.addMarker(MarkerOptions().position(location).title("Marker in location"))
-            }
-        }
-    }
-
     private fun init(context: Context, v : View) {
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
         Log.d("weatherWidget", "start of init")
 //        Log.d("City", "CITY = $CITY")
 
         val queue = Volley.newRequestQueue(context)
-        val url = "https://api.openweathermap.org/data/2.5/weather?q=$CITY&=Imperial&appid=$API"
+        val url = "https://api.openweathermap.org/data/2.5/weather?q=$CITY&units=imperial&appid=$API"
 
         val jsonObjectRequest = JsonObjectRequest(
             Request.Method.GET, url, null,
@@ -121,7 +97,7 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
             v.findViewById<TextView>(R.id.temperature).text = temp
             v.findViewById<TextView>(R.id.location).text = CITY
             v.findViewById<TextView>(R.id.conditions).text = conditions
-            Picasso.get().load(iconURL).resize(60,60).into(v.findViewById<ImageView>(R.id.weather_icon_iv))
+            Picasso.get().load(iconURL).resize(120,120).into(v.findViewById<ImageView>(R.id.weather_icon_iv))
 
         } catch (e: Exception) {
             Log.d("postApiCall Exception", "Exception = $e")
